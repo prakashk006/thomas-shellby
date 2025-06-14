9c34a23530672d9815a5ff328cc9d9e572b047d5
prakash <thangaprakashk894@gmail.com>
2025-06-14 15:56:39 +0530

    commit message

---
+++ javascript/script.js
@@ -0,0 +1,25 @@
+var a="20"
+var b=10
+console.log(a*b)
+console.log(typeof a)
+console.log(typeof b)
+
+ var mark=80;
+ var result = ( mark>=80)? "A":(mark>=60? "A+":(mark >=35? "p":"f"))
+ console.log(result)
+
+
+
+ let arr = [ 10,20,30,40]
+ let arr2 =[...arr,50,60,70]
+ console.log(arr)
+ console.log(arr2)
+
+
+var marks =[90,95,96,100,99]
+var [m1,m2,m3,m4,m5] = marks;
+console.log(m1)
+console.log(m2)
+console.log(m3)
+console.log(m4)
+console.log(m5)
